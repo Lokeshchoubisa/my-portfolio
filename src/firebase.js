c5e1480c03887f16f5fb13388590d48f0865d76e
Lokesh choubisa <68273275+Lokeshchoubisa@users.noreply.github.com>
2021-07-06 12:10:22 +0530

    backed updated

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,8 @@
 import firebase from "firebase"
 var firebaseConfig = {
     apiKey: "AIzaSyBoQZ35Y0PWdorHLxSaTufTh4ShCCFkmVY",
-    authDomain:process.env.REACT_APP_AUTHDOMAIN,
-    projectId:process.env.REACT_APP_PROJECTID,
+    authDomain:"my-portfolio-74167.firebaseapp.com",
+    projectId:"my-portfolio-74167",
     storageBucket: "my-portfolio-74167.appspot.com",
     messagingSenderId: "700114544177",
     appId: "1:700114544177:web:2c64b01ee4e791e9495710",
